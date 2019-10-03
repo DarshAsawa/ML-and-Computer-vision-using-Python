214a966940b0e189de2103cc9d882a808d160ce9
DarshAsawa <darshasawa@yahoo.co.in>
2019-10-03 23:36:38 +0530

    A basic Calculator

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+#Calculator Program
+print("\tWelcome To Python Calulator\n")
+
+#input of two integers from user
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+
+#to add the two entered numbers
+addition=a+b
+print(a," + ",b," = ",addition)
+
+#to find difference between two numbers
+difference=a-b
+print(a," - ",b," = ",difference)
+
+#Multiplication result of two numbers
+multiplication=a*b
+print(a," * ",b," = ",multiplication)
+
+#dividing the two numbers 
+division=a/b
+print(a," / ",b," = ",division)
+
+#finding modulo result of two numbers
+modulo = a%b
+print(a," % ",b," = ",modulo)
