3948738596216d82192ddca6fdb99af1a45667fc
DarshAsawa <darshasawa@yahoo.co.in>
2019-10-06 11:26:45 +0530

    Understanding the concept of Lists in Python via Calculating Avg Marks and Percentage of marks scored

---
+++ Avg_Marks.py
@@ -0,0 +1,23 @@
+
+# Program to calculate the average marks of your subjects
+print("\t\t Average Marks Calculator\n")
+
+#asking for number of subjects
+no_subjects=int(input("Enter the number of subjects:"))
+
+marks=[]
+for i in range(no_subjects):
+    mark=int(input("Enter marks for subject %d: " % (i+1)))
+    marks.append(mark)
+
+max_marks=100*no_subjects
+
+total=0
+
+for x in marks:
+    total+=x
+
+print("\nTotal marks out of %d is: %d" % (max_marks,total))
+
+Percentage=(total/max_marks)*100
+print("\nPercentage scored: %d % " % Percentage)
