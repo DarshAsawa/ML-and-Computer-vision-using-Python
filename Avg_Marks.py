b0951c978ef673ddf2fd21eb1bc6cf4175563de8
DarshAsawa <darshasawa@yahoo.co.in>
2019-10-09 18:40:53 +0530

    Adding comments for better understading and markings

--- Avg_Marks.py
+++ Avg_Marks.py
@@ -10,17 +10,20 @@ for i in range(no_subjects):
     mark=int(input("Enter marks for subject %d: " % (i+1)))
     marks.append(mark)
 
+#maximum marks that can be scored....
 max_marks=100*no_subjects
 
+#calculating the total marks scored....
 total=0
-
 for x in marks:
     total+=x
 
 print("\nTotal marks out of %d is: %d" % (max_marks,total))
 
+#calculating average marks....
 avg_marks=total/no_subjects
 print("\nAverage Mark scored is %d" % avg_marks)
 
+#calculating percentage scored....
 Percentage=(total/max_marks)*100
 print("\nPercentage scored: ",Percentage,"%")
